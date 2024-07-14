cc667042c121d622f2f3984a0d758d8754d28fc2
Russ <45741898+Vulp35@users.noreply.github.com>
2024-07-14 16:06:39 -0500

    Added uptime command

---
+++ commands/utility/uptime.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('uptime')
+        .setDescription('uptime of the bot'),
+    async execute(interaction) {
+        const botUptime = Math.floor((Date.now()/1_000) - process.uptime());
+        await interaction.reply({content:`I was started <t:${botUptime}:R>.`, ephemeral: true});
+    },
+
+
+}
